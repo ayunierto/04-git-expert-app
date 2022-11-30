063e635b75a6843f99522af775dc2caf24061393
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-11-30 05:09:19 -0500

    Crando lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+    console.log(categories)
+
   return (
     <>
         {/* Título */}
@@ -7,6 +14,11 @@ export const GifExpertApp = () => {
         {/* Input */}
 
         {/* Listado de Gif */}
+        <ol>
+            { categories.map( (category) => {
+                return <li key={ category }> { category } </li>
+            }) }
+        </ol>
             {/* GitItem */}
     </>
     )
