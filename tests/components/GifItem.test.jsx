56915e9a3a67228370b00d27d8d8c53cbd76c7b0
Alcides Yunier Turruellas Osorio <ayunierto@gmail.com>
2022-12-04 06:00:15 -0500

    Probando el componente GifItem.jsx

---
+++ tests/components/GifItem.test.jsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react'
+import { GifItem } from '../../src/components/GifItem';
+
+describe('Pruebas en el componente <GifItem />', () => { 
+
+    const title = 'Soy un titulo';
+    const url = 'https://google.com/';
+
+    test('Debe hace match con el snapshot', () => {
+        const { container } = render(<GifItem title={title} url={url} />);
+
+        expect( container ).toMatchSnapshot();
+    });
+
+    test('Debe mostrar la imagen con el url y el alt indicado', () => { 
+        render(<GifItem title={title} url={url} />);
+        // expect( screen.getByRole('img').src ).toBe( url );
+        // expect( screen.getByRole('img').alt ).toBe( title );
+
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    });
+
+    test('Debe de mostrar el titulo en el componente', () => { 
+        render(<GifItem title={title} url={url} />);
+        expect( screen.getByText( title ) ).toBeTruthy();
+     })
+
+ })
